d0a71d0774d7cbecd38e2f734dfa496ef8a5220b
Hanna <hanna.goncalves.pro@gmail.com>
2022-12-10 12:21:50 -0300

    rotas adicionadas

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express")
 const cors = require("cors")
 const database = require("./config/mongoConnect")
 const indexRouter = require("./routes/indexRoutes")
+const routerPosto = require('./routes/postoRoutes')
 
 const app = express()
 
@@ -10,6 +11,7 @@ app.use(express.json())
 app.use(cors())
 database.connect()
 
-app.use(indexRouter)
+//app.use(indexRouter)
+app.use(routerPosto)
 
 module.exports = app
--- src/routes/postoRoutes.js
+++ src/routes/postoRoutes.js
@@ -6,4 +6,15 @@ const controller = require('../controllers/postoController')
 const authController = require('../controllers/authController')
 const userController = require('../controllers/userController')
 
-//não tenho middlewares??
+const { checkAuth } = require('../middlewares/auth')
+
+router.post('/u/login', authController.login)
+router.post('/u/cadastro', userController.cadastraUser)
+
+router.get('/lista', controller.exibePosto)
+router.post('/p/cadasto', checkAuth, controller.cadastraPosto)
+router.get('/busca/:id', checkAuth, controller.buscaPostoPorId)
+router.patch('/p/atualizar/:id', checkAuth, controller.atualizaPosto)
+router.delete('/p/apagar', checkAuth, controller.apagaPosto)
+
+module.exports = router
